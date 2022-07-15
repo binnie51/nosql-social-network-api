57c42ffa93fc36a094fc1522e2fece5ce56e54b6
Vincent Tjia <vtjia51@gmail.com>
2022-07-15 18:48:42 -0400

    add thoughtRoutes to api

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,29 @@
 const router = require('express').Router();
 
 const {
-    
-}
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    postReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+// api/thoughts
+router.use('/')
+    .get(getThoughts)
+    .post(createThought);
+
+// api/thoughts/:thoughtId
+router.use('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// api/thoughts/:thoughtId/reactions
+router.use('/:thoughtId/reactions')
+    .post(postReaction)
+    .delete(deleteReaction);
+
+module.exports = router;
