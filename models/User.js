aca9a13003ffd8179dd1d296410f492eba364805
Vincent Tjia <vtjia51@gmail.com>
2022-07-15 20:03:10 -0400

    created User schema

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+// Schema for User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref : 'User',
+            }
+        ]
+    }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
